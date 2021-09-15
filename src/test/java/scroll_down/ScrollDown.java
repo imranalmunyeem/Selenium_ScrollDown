90944f43c40febfacba10613238303a1fd081a75
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2021-09-15 19:13:00 +0600

    Initial commit

---
+++ src/test/java/scroll_down/ScrollDown.java
@@ -0,0 +1,24 @@
+package scroll_down;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+import base_class.DriverSetup;
+
+public class ScrollDown extends DriverSetup {
+	@Test
+	public void ScrollDownInSelenium() throws InterruptedException {
+		
+		//Navigate to the Website
+		driver.get("https://celtabroad.com");
+		driver.manage().window().maximize();
+		
+		
+		
+		//Scroll Down
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("window.scrollBy(0,4500);");
+		Thread.sleep(1000);
+	
+		driver.close();
+	}
+}
